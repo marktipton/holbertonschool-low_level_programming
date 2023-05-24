4094f4362fd300a496b0f787bf5b3f797c4291c2
marktipton <6675@holbertonstudent.com>
2023-05-24 15:21:32 -0700

    betty white

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * main - the starting point for program execution in c
- * return - ends the execution of a function
+ *Return: ends the execution of a function
+ *description: print a string using the printf command 
  */
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art, ");
+printf ("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
 }
