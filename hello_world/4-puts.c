fcdf8abff5c556de3e07b97a1c41f9627939cc78
marktipton <6675@holbertonstudent.com>
2023-05-24 14:57:24 -0700

    :::::::{{{{{{[[[[[[[[]]]]]]]]]]

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main()
+/**
+ * main - the starting point for program execution in c
+ * return - ends the execution of a function
+ */
+int main(void)
 {
-puts "Programming is like building a multilingual puzzle
-return 0;
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+puts(Mystr);
+return (0);
 }
