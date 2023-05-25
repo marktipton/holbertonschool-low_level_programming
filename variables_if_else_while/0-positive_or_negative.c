df9a73f1b02b70a738df6130a18c0fd71c49e1c9
marktipton <6675@holbertonstudent.com>
2023-05-25 10:34:21 -0700

    betty mf holberton

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
+/**
  * main - this tells you whether n is greater than, less than, or equal to zero
+ *
+ * Return: Always 0
  */
 int main(void)
 {
