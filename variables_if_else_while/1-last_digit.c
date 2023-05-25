48e87e509e9eef179b9a3e982c364e42e7486663
marktipton <6675@holbertonstudent.com>
2023-05-25 11:13:23 -0700

    fnhell

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,15 +10,15 @@ int n, r;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 r = n % 10;
-if (n > 5)
+if (r > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, r);
 }
-if (n == 5)
+if (r == 5)
 {
 printf("Last digit of %d is %d and is 0\n", n, r);
 }
-if ((n < 6) && (n != 0))
+if ((r < 6) && (r != 0))
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, r);
 }
