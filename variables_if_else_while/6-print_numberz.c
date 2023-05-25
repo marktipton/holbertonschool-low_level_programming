2224741d7ff030aa76338c6815f985b00a5e9e52
marktipton <6675@holbertonstudent.com>
2023-05-25 12:34:32 -0700

    h

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -9,11 +9,11 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-int n = 48;
+int n = 0;
 /* your code goes there */
-while (n <= 57)
+while (n <= 9)
 {
-putchar(48 + n);
+putchar(0 + n);
 n++;
 }
 putchar('\n');
