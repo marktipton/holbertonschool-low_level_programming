fd042b6d839296b7ac0ab81a936c13dfce4808d4
marktipton <6675@holbertonstudent.com>
2023-05-24 23:01:34 -0700

    everything is bigger in texas

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,20 +6,17 @@
  */
 int main(void)
 {
- 
-    int intType;
-    float floatType;
-    char charType;
-    long int longintType;
-    long long int longlongintType;
-    
-    printf("Size of int: %zu bytes\n", sizeof(intType));
-    printf("Size of float: %zu bytes\n", sizeof(floatType));
-    printf("Size of longint: %zu bytes\n", sizeof(longintType));
-    printf("Size of char: %zu byte\n", sizeof(charType));
-    printf("Size of longlongint: %zu bytes\n", sizeof(longlongintType));
-    
-    return (0);
+int intType;
+float floatType;
+char charType;
+long int longintType;
+long long int longlongintType;
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of longint: %zu bytes\n", sizeof(longintType));
+printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of longlongint: %zu bytes\n", sizeof(longlongintType));
+return (0);
 }
 ~
 
