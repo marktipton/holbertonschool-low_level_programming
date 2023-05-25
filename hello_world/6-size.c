4d888f95aeebb373baf8e263683b3ae13fcbebe1
marktipton <6675@holbertonstudent.com>
2023-05-24 22:57:04 -0700

    trailing whitespace

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 /**
- * main - the starting point for program execution in c
- * return - ends the execution of a function
+ *main - the starting point for program execution in c
+ *Return: ends the execution of a function
+ *Description: displays the size of various types on the computer
  */
 int main(void)
 {
-char Mystr[] = "\"Programming is like building a multilingual puzzle";
-puts(Mystr);
-return (0);
+ 
+    int intType;
+    float floatType;
+    char charType;
+    long int longintType;
+    long long int longlongintType;
+    
+    printf("Size of int: %zu bytes\n", sizeof(intType));
+    printf("Size of float: %zu bytes\n", sizeof(floatType));
+    printf("Size of longint: %zu bytes\n", sizeof(longintType));
+    printf("Size of char: %zu byte\n", sizeof(charType));
+    printf("Size of longlongint: %zu bytes\n", sizeof(longlongintType));
+    
+    return (0);
 }
 ~
 
