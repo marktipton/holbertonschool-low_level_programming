fefa98c28b198ba247087410961f27eabc2e8bfa
marktipton <6675@holbertonstudent.com>
2023-05-25 12:17:18 -0700

    hello

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,21 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
+#include <stdio.h>
+/**
+ *main - somethings
+ *
+ * Return: always 0
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+int n = 0;
+/* your code goes there */
+while (n <= 9)
+{
+printf("%d", n);
+n++;
+}
+putchar('\n');
+return (0);
 }
