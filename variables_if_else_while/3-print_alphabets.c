4adb42b12122060740db46def9858d9e582e7009
marktipton <6675@holbertonstudent.com>
2023-05-25 11:56:53 -0700

    h

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,27 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
+#include <stdio.h>
+/**
+ *main - somethings
+ *
+ * Return: always 0
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+char alphabet = 'a';
+char uppercase = 'A';
+/* your code goes there */
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+while (uppercase <= 'Z')
+{
+putchar(uppercase);
+uppercase++;
+}
+putchar('\n');
+return (0);
 }
