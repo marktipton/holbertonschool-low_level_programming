31dad230495400e2d863eab8efd8089c6b392441
marktipton <6675@holbertonstudent.com>
2023-05-26 10:07:17 -0700

    nope

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - somethings
+ *
+ * Return: always 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	{
+_putchar(95);
+_putchar(112);
+_putchar(117);
+_putchar(116);
+_putchar(99);
+_putchar(104);
+_putchar(97);
+_putchar(114);
+_putchar('\n');
+return (0);
+	}
+}
