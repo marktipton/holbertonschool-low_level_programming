8bab163ca0989dca4282717dd91e50e05ebb6c49
marktipton <6675@holbertonstudent.com>
2023-05-26 11:07:39 -0700

    fnhl

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -10,7 +10,7 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	{		
+{
 _putchar(95);
 _putchar(112);
 _putchar(117);
@@ -21,5 +21,5 @@ _putchar(97);
 _putchar(114);
 _putchar('\n');
 return (0);
-	}
+}
 }
