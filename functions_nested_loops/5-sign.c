eafc52574933544172c2e362233f4eae6b2faf1e
marktipton <6675@holbertonstudent.com>
2023-05-28 16:28:23 -0700

    mel gibson

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,18 +10,20 @@
 /* betty style doc for function main goes there */
 int print_sign(int n)
 {
-char x = 97;
-int n = 0;
-while (n <=9)
+if (n > 0)
 {
-while (x <= 122)
-{
-_putchar(x);
-x++;
+return (1);
+_putchar('+');
 }
-n++;
+if (n == 0)
+{
+return (0);
+_putchar('0');
+}
+if (n < 0)
+{
+return (-1);
+_putchar('-');
 }
 _putchar('\n');
 }
-
-
