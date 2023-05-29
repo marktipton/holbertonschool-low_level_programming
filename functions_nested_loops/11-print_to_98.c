b272a6a26aef6c5d893fd31e25fa16369107c799
marktipton <6675@holbertonstudent.com>
2023-05-29 15:48:05 -0700

    putchar

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,27 +1,31 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 #include "main.h"
 /**
  *print_to_98 - somethings
  *
+ * @n: (take this integer and print every number between it and 98)
  * Return: always 0
  */
 /* betty style doc for function main goes there */
 void print_to_98(int n)
 {
+if (n == 98)
+{
+_putchar('0' + n);
+_putchar('\n');
+}
 while (n != 98)
 	{
 		if (n < 0)
 		{
 		_putchar('-');
-		return (n);
+		_putchar('0' + n);
 		_putchar(',');
 		_putchar(' ');
 		}
 		else
 		{	
-		return (n);
+		_putchar('0' + n);
 		_putchar(',');
                 _putchar(' ');
 
