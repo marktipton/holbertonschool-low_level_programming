11b65d98d84b67406cdbc3f2406b68da36816ea4
marktipton <6675@holbertonstudent.com>
2023-05-28 22:44:38 -0700

    new dawn new day

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,10 +8,7 @@
 /* betty style doc for function main goes there */
 void jack_bauer(void)
 {
-int a = 0;
-int b = 0;
-int c = 0;
-int d = 0;
+int a, b, c. d = 0;
 while (a < 3)
 {
 while (b < 10)
@@ -20,12 +17,6 @@ while (c < 6)
 {
 while (d < 10)
 {
-if ((a == 2) && (b == 3) && (c == 5) && (d == 9))
-{
-_putchar('\n');
-}
-else
-{
 _putchar('0' + a);
 _putchar('0' + b);
 _putchar(':');
@@ -33,22 +24,18 @@ _putchar('0' + c);
 _putchar('0' + d);
 d++;
 _putchar('\n');
-}
+if ((a == 2) && (b == 3) && (c == 5) && (d == 9))
+{
+goto newday;
 }
 c++;
 d = 0;
 }
-if ((a == 2) && (b == 3))
-{
-;
-}
-else
-{
 b++;
-}
 c = 0;
 }
 a++;
 b = 0;
 }
+newday;
 }
