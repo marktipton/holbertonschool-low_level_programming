aa6b87c4c0ee9c2126e07923e0b7ca01571a1521
marktipton <6675@holbertonstudent.com>
2023-05-28 21:05:33 -0700

    yes mr president!

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,25 +12,25 @@ void jack_bauer(void)
 {
 int n = 0;
 while (n < 3)
-	{
-	 _putchar(n);
-	while (n < 4)
-		{
-		 _putchar(n);
-		 _putchar(':');
-	  	while (n < 6)
-                	{
-			_putchar(n);
-			while (n < 10)
-                		{
-				_putchar(n);
-        			n++;
-                		}
-	 		n++;
-			}		
-        	n++;
-		}
-	n++;
-	}
+{
+_putchar(n);
+while (n < 4)
+{
+_putchar(n);
+_putchar(':');
+while (n < 6)
+{
+_putchar(n);
+while (n < 10)
+{
+_putchar(n);
+n++;
+}
+n++;
+}		
+n++;
+}
+n++;
+}
 _putchar('\n');
 }
