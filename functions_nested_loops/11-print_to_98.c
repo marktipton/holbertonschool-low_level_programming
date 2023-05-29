8545736962afa6301700041c1a4cca0a36d3fc79
marktipton <6675@holbertonstudent.com>
2023-05-29 11:19:40 -0700

    ?

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,17 +10,7 @@
 /* betty style doc for function main goes there */
 void print_to_98(int n)
 {
-char x = 97;
-int n = 0;
-while (n <=9)
-{
-while (x <= 122)
-{
-_putchar(x);
-x++;
-}
-n++;
-}
+return (n);
 _putchar('\n');
 }
 
