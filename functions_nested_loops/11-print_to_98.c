f2b744d0e9e7476ba50570c253e04cb6e1b50ace
marktipton <6675@holbertonstudent.com>
2023-05-29 15:38:36 -0700

    void

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,31 @@
 /* betty style doc for function main goes there */
 void print_to_98(int n)
 {
-return (n);
+while (n != 98)
+	{
+		if (n < 0)
+		{
+		_putchar('-');
+		return (n);
+		_putchar(',');
+		_putchar(' ');
+		}
+		else
+		{	
+		return (n);
+		_putchar(',');
+                _putchar(' ');
+
+		}
+	if (n > 98)
+	{
+	n--;
+	}
+	else
+	{
+	n++;
+	}
+	}
 _putchar('\n');
 }
 
