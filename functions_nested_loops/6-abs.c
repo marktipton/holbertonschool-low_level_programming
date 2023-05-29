945fe886cbbf3aae71a0fc594a8c3de7662918ef
marktipton <6675@holbertonstudent.com>
2023-05-28 19:20:29 -0700

    match

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -3,13 +3,12 @@
 /**
  *_abs - somethings
  *
- *@int: (take absv of this integer)
+ *@i: (take absv of this integer)
  *
  * Return: always 0
  */
 /* betty style doc for function main goes there */
-int i;
-int _abs(int)
+int _abs(int i)
 {
 _abs(i);
 _putchar('\n');
