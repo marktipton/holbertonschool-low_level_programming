9e7078383cdd1ef692bc1a6e0659b33a4a6c92ce
marktipton <6675@holbertonstudent.com>
2023-05-29 10:57:24 -0700

    first time's the charm?

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,17 +4,32 @@
 #include "main.h"
 /**
  *times_table - somethings
+ *@a: (multiplier 1)
+ *@b: (multiplier 2)
  *
  * Return: always 0
  */
 /* betty style doc for function main goes the*/
 void times_table(void)
 {
-int n = 0;
-while (n < 10)
-	{	
+int a = 0, b = 0;
+int c = (a * b);
+while (a < 10)
+	{
+	while (b < 10)
+	return (c);	
 	_putchar(',');
 	_putchar(' ');
+	if (c > 9)
+	{
+		_putchar(' ');
+	}
+	else
+	{
+		b++;
+	}
+	_putchar('\n');
+	a++;
 	}
 _putchar('\n');
 }
