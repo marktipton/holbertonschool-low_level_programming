f83e4eff8400b7967e59b36963521ed075a12c45
marktipton <6675@holbertonstudent.com>
2023-05-30 13:37:00 -0700

    ?

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,11 +1,9 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 #include "main.h"
 /**
  *times_table - somethings
- *@a: (multiplier 1)
- *@b: (multiplier 2)
+ *
+ *
  *
  * Return: always 0
  */
@@ -13,7 +11,7 @@
 void times_table(void)
 {
 int a = 0, b = 0;
-int c = (a * b);
+int c;
 while (a < 10)
 	{
 	while (b < 10)
@@ -37,7 +35,7 @@ while (a < 10)
 	}
 	_putchar('\n');
 	a++;
-	b = 0; 
+	b = 0;
 	}
 _putchar('\n');
 }
