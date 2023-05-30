f9e117ba6069b906ea7bf1bfbbfdbd4473ec7ec0
marktipton <6675@holbertonstudent.com>
2023-05-30 14:39:48 -0700

    ?

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,35 +15,29 @@ while (a < 10)
 	while (b < 10)
 	{
 	c = a * b;
-	if (c < 10)
-	{
-	_putchar(c + '0');
-	}
-	else
+	if (c > 9)
 	{
 	e = c % 10;
 	d = c / 10;
+	_putchar(',');
+	_putchar(' ');
 	_putchar(d + '0');
 	_putchar(e + '0');
 	}
-	if (b == 9)
-	{
-	_putchar('\n');
-	}
 	else
 	{
-	_putchar(',');
-	_putchar(' ');
+		if (b != 0)
+		{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		}
+		_putchar(c + '0');
 	}
 	b++;
-	c = a * b;
-	if (c < 10)
-	{
-	_putchar(' ');
 	}
-	}
-	b = 0;
 	a++;
+	_putchar('\n');
 	}
 }
 
