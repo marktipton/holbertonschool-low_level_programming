98dd405755e4b85ce74a472f3f4cd3ec392b02b6
marktipton <6675@holbertonstudent.com>
2023-05-31 18:47:37 -0700

    ?

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers from 0 to 9 
+ * print_numbers - print numbers from 0 to 9
  *
  * Return: Always 0.
  */
 void print_numbers(void)
 {
 	int x = 0;
+
 	while (x < 10)
 	{
 		_putchar(x + '0');
-		_putchar('\n');
 		x++;
 	}
+	_putchar('\n');
 }
