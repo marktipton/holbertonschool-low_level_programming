654a7c058700ef3671f4bf08a329f12c60f34487
marktipton <6675@holbertonstudent.com>
2023-05-31 18:55:04 -0700

    easier than last assignment somehow?

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,25 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_most_numbers - print 0 through 9 but skip 2 and 4
  *
  * Return: Always 0.
  */
-int main(void)
+void print_most_numbers(void)
 {
-    print_most_numbers();
-    return (0);
+	int c = 0;
+
+	while (c < 10)
+	{
+		if ((c == 2) || (c == 4))
+		{
+			c++;
+		}
+		else
+		{
+		_putchar(c + '0');
+		c++;
+		}
+	}
+	_putchar('\n');
 }
