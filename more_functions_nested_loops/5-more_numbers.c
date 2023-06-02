a7e595dd0860621ac657c2bc132c8e1c215bdff5
marktipton <6675@holbertonstudent.com>
2023-06-02 14:14:23 -0700

    ?

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,26 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * more_numbers - prints the numbers 0 to 14 ten times
  *
  * Return: Always 0.
  */
 void more_numbers(void)
 {
-	int x = 0, y;
+	int y =0, x = 0;
 
-	while(x < 15)
+while (x < 10)
+{
+	while(y < 15)
 	{
-		y = (x * 10);
-		_putchar(y + '0');
-		x++;
+		_putchar((y / 10) + '0');
+		if (y > 9)
+		{
+			_putchar((y % 10) + '0');
+		}
+		y++;
 	}
 	_putchar('\n');
+	x++;
+}
 }
