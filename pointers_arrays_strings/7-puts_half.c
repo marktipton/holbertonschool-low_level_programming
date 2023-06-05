5e529f484ae555e52ce671a704e79dcc7c41cbac
marktipton <6675@holbertonstudent.com>
2023-06-05 11:56:54 -0700

    in case of odd string length

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,14 @@ void puts_half(char *str)
 int n;
 int length = strlen(str);
 
-n = length / 2; 
+if (length % 2)
+{
+n = length / 2;
+}
+else
+{
+n = (length / 2) + 1;
+}
 while (str[n] != '\0')
 {
 	_putchar(str[n]);
