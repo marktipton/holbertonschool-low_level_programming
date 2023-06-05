90a27130117969b45b5fde60c8c516909ea8bc88
marktipton <6675@holbertonstudent.com>
2023-06-05 09:59:23 -0700

    ?

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,27 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * rev_string - reverses a string
  *
+ * @s: string to be reversed
  * Return: Always 0.
  */
 void rev_string(char *s)
 {
-    char s[10] = "My School";
+        static int i, len, temp;
+	len = strlen(s);
 
-    printf("%s\n", s);
-    rev_string(s);
-    printf("%s\n", s);
-    return (0);
+	if (i < len/2)
+        {
+                temp = s[i];
+		s[i] = s[len - i - 1];
+		temp = s[len - i - 1];
+		i++;
+		rev_string(s);
+	}
+        _putchar('\n');
+}
+
 }
