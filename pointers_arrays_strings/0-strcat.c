be58fa0261d0bfa54250777359190b6d3eeb7c17
marktipton <6675@holbertonstudent.com>
2023-06-07 11:20:39 -0700

    ?

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -4,23 +4,26 @@
 /**
  * _strcat - concatenates two strings
  *
+ * @dest: concatenated string
+ * @src: string to be tacked on to dest
  * Return: Always 0.
  */
 char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
 
-	while(dest[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		++dest;
 		i++;
 	}
-	while(dest[j] != '\0')
+	while (dest[j] != '\0')
 	{
 		*dest = *src;
 		dest++;
 		src++;
 		j++;
+		i++;
 	}
 	return (dest);
 }
