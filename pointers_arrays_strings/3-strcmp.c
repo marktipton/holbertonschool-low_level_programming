04ba57b373177a876f4645d637694a12919a8a87
marktipton <6675@holbertonstudent.com>
2023-06-07 14:38:27 -0700

    ?

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * _strcmp - compares two strings
  *@s1: string compare 1
  *
- *@s2; string compare 2
+ *@s2: string compare 2
  * Return: Always 0.
  */
 int _strcmp(char *s1, char *s2)
@@ -16,7 +16,7 @@ while (s1[i] == s2[i])
 {
 	i++;
 }
-if ((s1[i] + '0') < (s2[i] + '0'))
+/*if ((s1[i] + '0') < (s2[i] + '0'))
 {
 	return ((s1[i] + '0') - (s2[i] + '0'));
 }
@@ -25,7 +25,7 @@ if ((s1[i] + '0') > (s2[i] + '0'))
 	return ((s1[i] + '0') - (s2[i] + '0'));
 }
 else
-{
-	return (0);
-}
+{*/
+	return ((s1[i] + '0') - (s2[i] + '0'));
+
 }
