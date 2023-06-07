ee5d65d2e7e10bfe35800862af3e1fcf7210de08
marktipton <6675@holbertonstudent.com>
2023-06-07 11:39:11 -0700

    ?

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -14,15 +14,14 @@ char *_strcat(char *dest, char *src)
 
 	while (dest[i] != '\0')
 	{
-		++dest;
 		i++;
 	}
 	while (src[j] != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src[j];
 		j++;
+		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
