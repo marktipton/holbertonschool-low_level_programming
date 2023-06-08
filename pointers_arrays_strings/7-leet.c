8272c291656beb74045d3515b7c9ee5c6b36fbe9
marktipton <6675@holbertonstudent.com>
2023-06-08 12:55:42 -0700

    ?

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -11,8 +11,9 @@ char *leet(char *s)
 {
 
 int i = 0, j = 0;
-char Aletters[] = {97, 65, 101, 69, 111, 79, 116, 84, 108, 76};
-char Anumbers[] = {52, 51, 48, 55, 49, 52, 51, 48, 55, 49};
+char Alower[] = {97, 101, 111, 116, 108};
+char Aupper[] = {65, 69, 79, 84, 76};
+char Anumbers[] = {52, 51, 48, 55, 49};
 int size = 10;
 
 while (s[i] != '\0')
@@ -20,7 +21,7 @@ while (s[i] != '\0')
 	j = 0;
 	while (j < size - 1)
 	{
-		if (s[i] == Aletters[j])
+		if (s[i] == Alower[j] || s[i] == Aupper[j])
 		{
 			s[i] = Anumbers[j];
 		}
