4e47d9839dad4f6016e2972a163fbc5708f21706
marktipton <6675@holbertonstudent.com>
2023-06-07 17:09:47 -0700

    ?

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -4,15 +4,24 @@
 /**
  * cap_string - capitalizes all words in a string
  *
+ * @s: capitalize words of this string
  * Return: Always 0.
  */
-char *cap_string(char *)
+char *cap_string(char *s)
 {
-    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
-
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
+int i = 0, j = 0;
+char no[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
+while (s[i] != '\0')
+{
+	while (
+	if (s[i] == no[j])
+	{
+		if (s[i+1] >= 97 && s[i+1] <= 122)
+		{
+			s[i+1] = s[i+1] - 32;
+		}	
+		
+	}
+	i++;
+}
 }
