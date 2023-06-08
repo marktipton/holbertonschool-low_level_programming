72309f2aed38dea16e26d1b343e3c801efc7fb26
marktipton <6675@holbertonstudent.com>
2023-06-08 12:56:53 -0700

    ?

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -14,7 +14,7 @@ int i = 0, j = 0;
 char Alower[] = {97, 101, 111, 116, 108};
 char Aupper[] = {65, 69, 79, 84, 76};
 char Anumbers[] = {52, 51, 48, 55, 49};
-int size = 10;
+int size = 5;
 
 while (s[i] != '\0')
 {
