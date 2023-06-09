6a18c7828b2f9f9a9391064c1739108f5f1f1497
marktipton <6675@holbertonstudent.com>
2023-06-09 14:16:14 -0700

    ?

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -13,10 +13,11 @@ char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i = 0;
 
-while ((s[i] != '\0') && (i <= n))
+while ((s[i] != '\0') && (i < n))
 {
 	s[i] = b;
 	i++;
 }
+s[i] = b;
 return (s);
 }
