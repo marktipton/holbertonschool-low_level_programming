2d528ba5c21d157af4dc720201a3e23a4079f6fd
marktipton <6675@holbertonstudent.com>
2023-06-10 15:55:03 -0700

    ?

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -9,12 +9,12 @@
  */
 void print_chessboard(char (*a)[8])
 {
-int i = 0, j = 0;
+int i = 0;
+int j;
 
 	while (i < 8)
 	{
-		
-	}
+		j = 0;
 		while (j < 8)
 		{
 			_putchar(a[i][j]);
@@ -22,4 +22,5 @@ int i = 0, j = 0;
 		}
 	_putchar('\n');
 	i++;
+	}
 }
