b33302de44a8e8e5857849f7f556571354f7358d
marktipton <6675@holbertonstudent.com>
2023-06-10 14:47:15 -0700

    ?

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,15 +12,15 @@ char *_strchr(char *s, char c)
 int i = 0;
 
 
-while (*s != '\0')
+while (s[i] != '\0')
 {
-	if (*s == c)
+	if (s[i] == c)
 	{
 		return (s);
 	}
 	i++;
 }
-if (*s == '\0')
+if (s[i] == '\0')
 {
 	return (NULL);
 }
