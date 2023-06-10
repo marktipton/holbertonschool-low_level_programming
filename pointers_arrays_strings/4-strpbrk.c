c169512a0d651aff983af0ea732119858c6d3652
marktipton <6675@holbertonstudent.com>
2023-06-10 14:26:00 -0700

    there is no bloody way in hell that this is right but you have to start somewhere

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -10,5 +10,24 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
+	int i = 0;
+	int j = 0;
 
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				return (s);
+			}
+					j++;
+		}
+		i++;
+	}
+	if (s[i] == '\0')
+	{
+	return NULL;
+	}
+	return (0);
 }
