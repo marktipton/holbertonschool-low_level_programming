46c8a43ce0b20f5cb0ca698ffbe0d97a5b5581a1
marktipton <6675@holbertonstudent.com>
2023-06-11 11:42:47 -0700

    ?

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,26 +1,32 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - check the code
- *
+ * print_diagsums - prints the sum of the two diagonals of a square
+ *@a: square array
+ *@size: size of square
  * Return: Always 0.
  */
-int main(void)
+void print_diagsums(int *a, int size)
 {
-    int c3[3][3] = {
-        {0, 1, 5},
-        {10, 11, 12},
-        {1000, 101, 102},
-    };
-    int c5[5][5] = {
-        {0, 1, 5, 12124, 1234},
-        {10, 11, 12, 123521, 12512},
-        {1000, 101, 102, 12545, 214543435},
-        {100, 1012451, 11102, 12545, 214543435},
-        {10, 12401, 10452, 11542545, 1214543435},
-    };
-    print_diagsums((int *)c3, 3);
-    print_diagsums((int *)c5, 5);
-    return (0);
+int i = 0;
+int j = 0;
+int sumdiag = 0;
+
+while (i < size)
+{
+	sumdiag = sumdiag + a[i][j];
+	j++;
+	i++;
+}
+i = 0;
+while (i < size)
+{
+	sumdiag = sumdiag + a[i][j];
+	j--;
+	i++;
+
+}
+printf("%d", sumdiag);
 }
