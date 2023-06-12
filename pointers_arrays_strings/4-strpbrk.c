e1bcae38853ae99e76bfd0c0d37be1509453fa4d
marktipton <6675@holbertonstudent.com>
2023-06-12 13:50:06 -0700

    ?

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -11,23 +11,21 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i = 0;
-	int j = 0;
+	int j;
 
 	while (s[i] != '\0')
 	{
+		j = 0;
 		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
 			{
 				return (&s[i]);
 			}
-					j++;
+			j++;
 		}
 		i++;
 	}
-	if (s[i] == '\0')
-	{
 	return (NULL);
-	}
-	return (0);
+	
 }
