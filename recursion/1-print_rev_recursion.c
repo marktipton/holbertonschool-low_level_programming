e35a3c36b8414d50f8808560cb97342acfa37215
marktipton <6675@holbertonstudent.com>
2023-06-14 14:23:29 -0700

    ?

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * _print_rev_recursion - recursively prints string in reverse
+ *@s: string to be reversed
  * Return: Always 0.
  */
-int main(void)
+void _print_rev_recursion(char *s)
 {
-    _print_rev_recursion("\nColton Walker");
-    return (0);
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
 }
