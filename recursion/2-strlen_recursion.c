17f80aaea3d25c702d835654ec7609340d62092f
marktipton <6675@holbertonstudent.com>
2023-06-14 14:50:09 -0700

    ?

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -10,14 +10,10 @@ int _strlen_recursion(char *s)
 {
 int len = 0;
 
-if (*s == '\0')
-{
-	return (len + 1);
-}
-else
+if (*s != '\0')
 {
 	len++;
 	_strlen_recursion(s + 1);
 }
-return (0);
+return (len + 1);
 }
