d58d4afefc020b8ec67717a5bf5ec3629eb4bee6
marktipton <6675@holbertonstudent.com>
2023-06-14 16:17:37 -0700

    ?

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -9,10 +9,14 @@
  */
 int _pow_recursion(int x, int y)
 {
-if (y > 0)
+if (y > 1)
 {
 	y--;
-        return (x * _pow_recursion(x, y));
+        return (x * _pow_recursion(x, y - 1));
+}
+if (y == 0)
+{
+	return (1);
 }
 
 return (0);
