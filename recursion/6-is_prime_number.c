4724c670fa7193c2084d9cc2f80956065310c58c
marktipton <6675@holbertonstudent.com>
2023-06-15 11:03:49 -0700

    ?

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -5,20 +5,21 @@
  *@n: number
  *@i: divisor
  *
+ * Return: Always 0.
  */
 int calc(int n, int i)
 {
 if ((n % i == 0) && (i < n))
 {
-	return (1);
+	return (0);
 }
 else if (n == i)
 {
-	return (0);
+	return (1);
 }
 else
 {
-	return calc(n, i + 1);
+	return (calc(n, i + 1));
 }
 }
 
@@ -37,6 +38,6 @@ int is_prime_number(int n)
 	if (n == 1)
 	{
 		return (1);
-	}	
+	}
 	return (calc(n, 2));
 }
