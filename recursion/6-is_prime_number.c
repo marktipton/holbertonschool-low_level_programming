4bcc75dac2677eb2c3113bb041b08bbe995b1463
marktipton <6675@holbertonstudent.com>
2023-06-15 10:54:47 -0700

    ?

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,5 +1,26 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * calc - returns 1 if prime
+ *@n: number
+ *@i: divisor
+ *
+ */
+int calc(int n, int i)
+{
+if ((n % i == 0) && (i < n))
+{
+	return (1);
+}
+else if (n == i)
+{
+	return (0);
+}
+else
+{
+	return calc(n, i + 1);
+}
+}
 
 /**
  * is_prime_number - returns 1 if the input is a prime number
@@ -8,13 +29,14 @@
  */
 int is_prime_number(int n)
 {
-	int i;
 
-	i * i = n;
-
-	if ()
+	if (n <= 0)
+	{
+		return (0);
+	}
+	if (n == 1)
 	{
 		return (1);
 	}	
-	return (0);
+	return (calc(n, 2));
 }
