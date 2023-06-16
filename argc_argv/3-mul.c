684d34ea71107d43e845e027a31383f2d241af05
marktipton <6675@holbertonstudent.com>
2023-06-16 11:45:38 -0700

    >

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,1 +1,19 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *main - multiplies two numbers
+ *
+ *@
+ *@
+ *
+ * Return: always 0.
+ */
+
+int main(int x, int y)
+{
+	int z = x * y;
+
+	printf("%d\n", z);
+}
