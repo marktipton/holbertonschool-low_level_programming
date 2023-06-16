83df20bc6dd3a830b58e601c3f45a82f083a7ca1
marktipton <6675@holbertonstudent.com>
2023-06-16 13:29:01 -0700

    ?

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,1 +1,15 @@
 #include "main.h"
+
+/**
+ *main - adds positive integers. 
+ *
+ *@argc: number of arguments
+ *@argv: array of argument strings
+ *
+ *Return: always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	
+}
