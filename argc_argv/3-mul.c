4dcda59140725281d1814630baa09af0387247eb
marktipton <6675@holbertonstudent.com>
2023-06-16 12:36:02 -0700

    ?

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -5,15 +5,25 @@
  *
  *main - multiplies two numbers
  *
- *@
- *@
+ *@x: multiplier
+ *@y: also multiplier
  *
  * Return: always 0.
  */
 
-int main(int x, int y)
+int main(int argc, char *argv[])
 {
+	int x = *argv[1];
+	int y = *argv[2];
 	int z = x * y;
 
+
+	if (argc - 1 < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	printf("%d\n", z);
+	return (0);
+
 }
