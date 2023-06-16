a746f8248e55c1f5fb7e251ecb10bdcf75b8d885
marktipton <6675@holbertonstudent.com>
2023-06-16 11:23:19 -0700

    ?

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- *
- *main - prints the number of arguments passed
+ * main - prints the number of arguments passed
  *
  *@argc: number of arguments
  *@argv: array of argument strings
@@ -14,6 +14,6 @@ int main(int argc, char *argv[])
 {
 	(void)argv;
 
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
