d32a47aab3f14ac81ca14d9fe859c57d7dcced30
marktipton <6675@holbertonstudent.com>
2023-06-19 11:48:51 -0700

    ?

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,9 +12,7 @@
 
 int main(int argc, char *argv[])
 {
-	int x = atoi(argv[1]);
-	int y = atoi(argv[2]);
-	int z = x * y;
+	int x, y, z;
 
 
 	if (argc != 3)
@@ -22,10 +20,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
+	
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	z = x * y;
+
 	printf("%d\n", z);
-	}
 	return (0);
 
 }
