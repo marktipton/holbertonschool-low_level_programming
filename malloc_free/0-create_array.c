a396f63c4d5e7be778057cdf607f2d9d584c9da8
marktipton <6675@holbertonstudent.com>
2023-06-21 10:54:51 -0700

    ?

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,18 +11,22 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	int i = 0, j;
+	unsigned int i = 0;
+	char *array = malloc(size * sizeof(char));
 
+	if (size == 0)
+	{
+		return (NULL);		
+	}
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 	while (i < size)
 	{
-		j = 0;
-		while (j < size)
-		{
-			_putchar(c);
-			j++;
-		}
-		_putchar('\n');
+		array[i] = c;
 		i++;
 	}
+	return (array);
 
 }
