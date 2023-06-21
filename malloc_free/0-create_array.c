da89ddc5faba708cf39950c2b31d06524813276c
marktipton <6675@holbertonstudent.com>
2023-06-21 09:32:25 -0700

    made files

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of characters
+ * @c: specific character
+ * @size: size of array
+ *
+ * Return: pointer to array or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+
+}
