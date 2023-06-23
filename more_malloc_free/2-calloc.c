b9d7211141dfc59a353f1463736e59c54e8a6466
marktipton <6675@holbertonstudent.com>
2023-06-23 14:05:26 -0700

    ?

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -22,6 +22,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	memset(ptr, 0, nmemb *size);
+	memset(ptr, 0, nmemb * size);
 	return (ptr);
 }
