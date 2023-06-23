ab29e26c07f7f9dc2319fb76f2d0eb3b0904ead5
marktipton <6675@holbertonstudent.com>
2023-06-23 10:04:29 -0700

    ?

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,19 +1,34 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * string_nconcat - check the code
+ *@s1: string 1
+ *@s2: string 2
+ *@n: number of bytes to take from string 2
+ * Return: concatenated string or NULL
  */
-int main(void)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *concat;
+	size_t len1 = strlen(s1);
+	char *s3 = malloc((len1 + n + 1) * sizeof(char));
 
-    concat = string_nconcat("Best ", "School !!!", 6);
-    printf("%s\n", concat);
-    free(concat);
-    return (0);
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	if (s3 == NULL)
+	{
+		return (NULL);
+	}
+	strncpy(s3, s1, len1);
+	strncpy(s3 + len1, s2, n);
+	s3[len1 + n] = '\0';
+	return (s3);
 }
-
