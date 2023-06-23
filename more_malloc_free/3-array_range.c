ed0f140a2a22819fe18941e41308a2524ef1d573
marktipton <6675@holbertonstudent.com>
2023-06-23 14:38:36 -0700

    ?

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -14,18 +14,20 @@ int *array_range(int min, int max)
 {
 	int *ptr;
 	int i;
+	int j;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
-	ptr = malloc(abs(max - min + 1) * sizeof(int));
+	ptr = malloc((max - min + 1) * sizeof(int));
 	if (ptr == NULL)
 		return (NULL);
-
+	j = 0;
 	for (i = min; i <= max; i++)
 	{
-		ptr[i] = i;
+		ptr[j] = i;
+		j++;
 	}
 	return (ptr);
 }
