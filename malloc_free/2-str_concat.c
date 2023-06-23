810f67430057fe125a1e09a355e5abb3a754cd33
marktipton <6675@holbertonstudent.com>
2023-06-23 10:38:24 -0700

    ?

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,8 +11,8 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	size_t len1 = strlen(s1);
-	size_t len2 = strlen(s2);
+	size_t len1;
+	size_t len2;
 	char *s3;
 
 	if (s1 == NULL)
@@ -23,6 +23,8 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
 	s3 = malloc((len1 + len2 + 1) * sizeof(char));
 
