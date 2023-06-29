695608cf05e6b6e8d7409378c2c7c6cd9e1c9d60
marktipton <6675@holbertonstudent.com>
2023-06-29 12:52:38 -0700

    ?

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,12 +2,12 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name as is
  * @name: name of the person
- *
+ *@f: void pointer to print_name_as_is function
  * Return: Nothing.
  */
-void print_name_as_is(char *name)
+void print_name(char *name, void (*f)(char *))
 {
-    printf("Hello, my name is %s\n", name);
+
 }
