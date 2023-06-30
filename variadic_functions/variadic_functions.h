ecc29ffeb94950efba773c6dd6061a3019e35fdd
marktipton <6675@holbertonstudent.com>
2023-06-30 12:59:37 -0700

    files created

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef _VARFUNC_
+#define _VARFUNC_
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
