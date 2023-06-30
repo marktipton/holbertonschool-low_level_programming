4a68291d6634c97de4fd99a389afaa2d2018ccd8
marktipton <6675@holbertonstudent.com>
2023-06-30 11:47:34 -0700

    made function comments

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,48 +1,51 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- *
- *
- *
- *
+ *op_add - adds integers
+ *@a: int 1
+ *@b: int 2
+ *Return: sum
  */
 int op_add(int a, int b)
 {
 
 }
 /**
- *
- *
- *
- *
+ *op_sub - subtract integers
+ *@a: int 1
+ *@b: int 2
+ *Return: difference
  */
 int op_sub(int a, int b)
 {
 
 }
 /**
- *
- *
- *
- *
+ *op_mul - multiplies integers
+ *@a: int 1
+ *@b: int 2
+ *Return: product
  */
 int op_mul(int a, int b)
 {
 
 }
 /**
- *
- *
- *
- *
+ *op_div - divides numbers
+ *@a: int 1
+ *@b: int 2
+ *Return: quotient
  */
 int op_div(int a, int b)
 {
 
 }
 /**
- *
- *
- *
- *
+ *op_mod - gets remainder of division
+ *@a: int 1
+ *@b: int 2
+ *Return: remainder
  */
 int op_mod(int a, int b)
 {
