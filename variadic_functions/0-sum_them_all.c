787c91c104e3681fa0279e3a927c86a1def2ca1f
marktipton <6675@holbertonstudent.com>
2023-06-30 13:38:26 -0700

    one function to sum them all

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
- * main - check the code
+ * sum_them_all - sums all of its parameters
  *
- * Return: Always 0.
+ * Return: 0 if n == 0, otherwise returns the sum
  */
-int main(void)
+int sum_them_all(const unsigned int n, ...)
 {
-    int sum;
+	va_list ap;
+	int i, j;
 
-    sum = sum_them_all(2, 98, 1024);
-    printf("%d\n", sum);
-    sum = sum_them_all(4, 98, 1024, 402, -1024);
-    printf("%d\n", sum);    
-    return (0);
+	if (n == 0)
+		return (0);
+	
+	va_start(ap, n);
+	i = 0;
+
+	for (i = 0; i < n; i++)
+	{	
+		j += va_arg(ap, unsigned int);
+	}
+	va_end(ap);
+	return n;
 }
