5fe71b0f798e6766a866db6fd14f111d53b5a6aa
marktipton <6675@holbertonstudent.com>
2023-07-03 13:32:21 -0700

    ?

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,3 +1,7 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  *get_op_func - selects function to perform
  *
@@ -21,7 +25,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (i <  5)
 	{
-		if (ops[i] == s)
+		if (strcmp(ops[i].op, s) == 0)
 		{
 			return (ops[i].f);	
 		}
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,35 +1,42 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
+ * main - convert arguments into int
+ *@argc: argument count
+ *@argv: argument array
  *
- *
- *
- *
+ *Return: operation result
  */
 
-int main(int __attribute__((unused)) argc, *argv[])
+int main(int argc, char **argv)
 {
+	int num1;
+	char *operator;
+	int num2;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit (98);
 	}
+	
+	num1 = atoi(argv[2]);
+	operator = argv[3];
+	num2 = atoi(argv[3]);
 
-	int a = atoi(num1);
-	int b = atoi(num2);
-
-	if (num2 == 0 && (operator == "/" || operator == "%"))
+	if (num2 == 0 && (strcmp(operator, "&") == 0 || strcmp(operator, "%") == 0))
 	{
 		printf("Error\n");
 		exit (100);
 		
 	}
-	if (get_op_func(a, b) == NULL)
+	if (get_op_func(operator) == NULL)
 	{
 		printf("Error\n");
 		exit (99);
 	}
-	return (get_op_func(a, b));
+	printf("%d\n", get_op_func(operator)(num1, num2));
+return(0);
 }
