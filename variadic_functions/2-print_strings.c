12b5461824cfcf1ef862141766fe567b3fcd81f3
marktipton <6675@holbertonstudent.com>
2023-07-03 10:08:44 -0700

    ?

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (va_arg(ap, char *) == NULL)
 			printf("(nil)");
 		printf("%s", va_arg(ap, char *));
-		if (separator != NULL && (i != n - 1))
+		if (separator != NULL || (i != n - 1))
 			printf("%s", separator);
 	}
 	va_end(ap);
