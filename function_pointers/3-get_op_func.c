60e546f1c720a59691564be83dab137aefa4d7d9
marktipton <6675@holbertonstudent.com>
2023-07-03 14:15:50 -0700

    ?

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -24,7 +24,7 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (ops[i].op != NULL && strcmp(ops[i].op, s) != 0)
+	while (ops[i].op != NULL && strcmp(*(ops[i].op), *s) != 0)
 	{
 		i++;
 	}
