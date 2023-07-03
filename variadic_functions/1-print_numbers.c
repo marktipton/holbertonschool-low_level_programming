29bb9999c8d40851134a11e60f2f82c34d31e80b
marktipton <6675@holbertonstudent.com>
2023-07-03 09:45:33 -0700

    ?

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, const unsigned int));
-		if (separator != NULL)
+		if (separator != NULL && (i != n - 1))
 			printf("%s", separator);
 	}
 	va_end(ap);
