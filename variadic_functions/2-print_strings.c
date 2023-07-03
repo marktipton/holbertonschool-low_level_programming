3eaf58bd6fc271dd457bdf1eea64afe74c34f9ff
marktipton <6675@holbertonstudent.com>
2023-07-03 10:04:30 -0700

    character pointer

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,12 +1,29 @@
 #include "variadic_functions.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 /**
- * main - check the code
- *
+ * print_strings - prints stringss followed by a new line
+ *@separator: string to be printed between strings
+ *@n: number of integers passed to function
  * Return: Always 0.
  */
-int main(void)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-    print_strings(", ", 2, "Jay", "Django");
-    return (0);
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if (va_arg(ap, char *) == NULL)
+			printf("(nil)");
+		printf("%s", va_arg(ap, char *));
+		if (separator != NULL && (i != n - 1))
+			printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");
 }
