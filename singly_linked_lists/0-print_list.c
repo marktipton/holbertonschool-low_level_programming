404e59436cfdd24f7e27c57c6e77d9261439093d
marktipton <6675@holbertonstudent.com>
2023-07-12 14:23:55 -0700

    fixed whitespaces

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,14 +12,14 @@
 size_t print_list(const list_t *h)
 {
 	int i;
-	
+
 	if (h == NULL)
-		return (0); 
+		return (0);
 	for (i = 1; h->next != NULL; i++)
 	{
 		if (h->str == NULL)
 			printf("[%u} %s\n", h->len, "(nil)");
-		else 
+		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 	}
