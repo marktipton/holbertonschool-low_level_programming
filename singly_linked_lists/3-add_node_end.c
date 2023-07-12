a3c23f2284204ef02c142a06b56353e9ad7c3a5c
marktipton <6675@holbertonstudent.com>
2023-07-12 11:45:03 -0700

    made file prototypes

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -4,43 +4,13 @@
 #include "lists.h"
 
 /**
- * main - check the code
+ * add_node_end - adds node to end of list_t list
  *
- * Return: Always 0.
+ * @head: first node in list
+ * @str: string to be duplicated
+ * Return: the address of the new element or NULL if failed
  */
-int main(void)
+list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *head;
 
-    head = NULL;
-    add_node_end(&head, "Anne");
-    add_node_end(&head, "Colton");
-    add_node_end(&head, "Corbin");
-    add_node_end(&head, "Daniel");
-    add_node_end(&head, "Danton");
-    add_node_end(&head, "David");
-    add_node_end(&head, "Gary");
-    add_node_end(&head, "Holden");
-    add_node_end(&head, "Ian");
-    add_node_end(&head, "Ian");
-    add_node_end(&head, "Jay");
-    add_node_end(&head, "Jennie");
-    add_node_end(&head, "Jimmy");
-    add_node_end(&head, "Justin");
-    add_node_end(&head, "Kalson");
-    add_node_end(&head, "Kina");
-    add_node_end(&head, "Matthew");
-    add_node_end(&head, "Max");
-    add_node_end(&head, "Michael");
-    add_node_end(&head, "Ntuj");
-    add_node_end(&head, "Philip");
-    add_node_end(&head, "Richard");
-    add_node_end(&head, "Samantha");
-    add_node_end(&head, "Stuart");
-    add_node_end(&head, "Swati");
-    add_node_end(&head, "Timothy");
-    add_node_end(&head, "Victor");
-    add_node_end(&head, "Walton");
-    print_list(head);
-    return (0);
 }
--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -4,22 +4,12 @@
 #include "lists.h"
 
 /**
- * main - check the code
+ * free_list - check the code
  *
+ * @head: 
  * Return: Always 0.
  */
-int main(void)
+void free_list(list_t *head)
 {
-    list_t *head;
 
-    head = NULL;
-    add_node_end(&head, "Bob");
-    add_node_end(&head, "&");
-    add_node_end(&head, "Kris");
-    add_node_end(&head, "love");
-    add_node_end(&head, "asm");
-    print_list(head);
-    free_list(head);
-    head = NULL;
-    return (0);
 }
