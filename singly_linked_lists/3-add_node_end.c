0cf91795300dd04a0eddf986aa0dd7b6a534bc19
marktipton <6675@holbertonstudent.com>
2023-07-12 16:25:11 -0700

    ?

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -53,7 +53,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->str = dup;
 	new_node->len = len;
 	new_node->next = NULL;
-	if(*head == NULL)
+	if (*head == NULL)
 	{
 		*head = new_node;
 		return (*head);
