e500ee10765b84a25410ff71504d6a8662e1e164
marktipton <6675@holbertonstudent.com>
2023-07-12 15:58:58 -0700

    add new node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,13 +4,53 @@
 #include "lists.h"
 
 /**
+  * _strlen - gets length of the string
+  * @s: string
+  * Return: length of the string
+  */
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+/**
  * add_node - check the code
  *
  * @head: first node
  * @str: string to duplicate
  * Return: Address of new element of NULL if it failed
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node;
+	char *dup;
+	int i, len;
 
+	if (str == NULL || head == NULL)
+		return (NULL);
+	len = _strlen(str);
+	new_node = *head;
+
+	dup = malloc((len + 1) * sizeof(char));
+	if (dup == NULL)
+		return (NULL);
+
+        for (i = 0; str[i]; i++)
+		dup[i] = str[i];
+	new_node = malloc(sizeof(struct list_t));
+
+	if (new_node == NULL)
+	{
+		free(dup);
+		return (NULL);
+	}
+	new_node->str = dup;
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
