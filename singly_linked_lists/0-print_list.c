49d3d8e3b7ded1fb8eacaa6c063358b66f52cba1
marktipton <6675@holbertonstudent.com>
2023-07-12 14:52:25 -0700

    fixed }

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -18,11 +18,11 @@ size_t print_list(const list_t *h)
 	for (i = 1; h->next != NULL; i++)
 	{
 		if (h->str == NULL)
-			printf("[%u} %s\n", h->len, "(nil)");
+			printf("[%u] %s\n", h->len, "(nil)");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 	}
 	printf("[%u] %s\n", h->len, h->str);
-	return(i);
+	return (i);
 }
