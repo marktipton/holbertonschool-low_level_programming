92518e1830a48b6467e2cd27df796e3735bf29e3
marktipton <6675@holbertonstudent.com>
2023-07-12 11:21:29 -0700

    made files

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints a list of the elements of list_t
+ *
+ * @h: pointer to list elements
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+
+}
---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - list number of elements in linked list list_t
+ *
+ * @h: 
+ * Return: number of elements in linked list
+ */
+size_t list_len(const list_t *h)
+{
+
+}
