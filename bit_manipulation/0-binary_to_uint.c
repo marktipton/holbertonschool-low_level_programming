d2ae12548f15af650ae6408258f850fe22807363
marktipton <6675@holbertonstudent.com>
2023-07-13 12:47:12 -0700

    convert binary to unsiged int

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
 #include "main.h"
+#include <math.h>
+#include <string.h>
 
 /**
- * main - check the code
+ * binary_to_uint - converts binary number to unsigned int
  *
+ * @b: pointer to a string of 0's and 1's
  * Return: Always 0.
  */
-int main(void)
+unsigned int binary_to_uint(const char *b)
 {
-    unsigned int n;
+	unsigned int len;
+	unsigned int i;
+	unsigned int sum;
 
-    n = binary_to_uint("1");
-    printf("%u\n", n);
-    n = binary_to_uint("101");
-    printf("%u\n", n);
-    n = binary_to_uint("1e01");
-    printf("%u\n", n);
-    n = binary_to_uint("1100010");
-    printf("%u\n", n);
-    n = binary_to_uint("0000000000000000000110010010");
-    printf("%u\n", n);
-    return (0);
+	len = strlen(b);
+	i = 1;
+	sum = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (; len - 1 > 0; len--)
+	{
+		if (b[len] != '1' || b[len] != '0')
+			return (0);
+		if (b[len] == '1')
+			sum += pow(2, i);
+		i++;	
+	}	
+	return (sum);
 }
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int _putchar(char c)
+int _putchar(char c);
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
