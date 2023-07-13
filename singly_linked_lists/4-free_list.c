e497785b47a8818f635256910fd8c48c18f16a97
marktipton <6675@holbertonstudent.com>
2023-07-13 10:31:24 -0700

    free list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -6,10 +6,14 @@
 /**
  * free_list - check the code
  *
- * @head: 
+ * @head: first node 
  * Return: Always 0.
  */
 void free_list(list_t *head)
 {
-
+	if (head == NULL)
+		return;
+	free_list(head->next);
+	free(head->str);
+	free(head);
 }
