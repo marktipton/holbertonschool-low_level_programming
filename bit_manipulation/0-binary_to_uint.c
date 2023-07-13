48162b8497fc0f1a19fda297be8693e45fd224df
marktipton <6675@holbertonstudent.com>
2023-07-13 13:05:15 -0700

    <math.h> doesnt work

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -7,28 +7,34 @@
  * binary_to_uint - converts binary number to unsigned int
  *
  * @b: pointer to a string of 0's and 1's
- * Return: Always 0.
+ * Return: sum or 0 if null or nonbinary character
  */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int len;
 	unsigned int i;
 	unsigned int sum;
+	unsigned int k;
 
-	len = strlen(b);
+	len = strlen(b) - 1;
 	i = 1;
 	sum = 0;
+	k = len;
 
 	if (b == NULL)
 		return (0);
 
-	for (; len - 1 > 0; len--)
+	for (; k > 0; k--)
 	{
-		if (b[len] != '1' || b[len] != '0')
+		if (b[k] != '1' || b[k] != '0')
 			return (0);
-		if (b[len] == '1')
-			sum += pow(2, i);
-		i++;	
+		
+		while (i <= len && b[k] == '1')
+		{
+			
+			sum *= 2
+			i++;
+		}	
 	}	
 	return (sum);
 }
