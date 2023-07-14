b55a3d5b3e8db63908b67ee82615458f8a8f65fe
marktipton <6675@holbertonstudent.com>
2023-07-13 17:19:24 -0700

    >> &&&&&&&

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include "main.h"
-
+#include "_pow.c"
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+void print_binary(unsigned long int n)
 {
-    print_binary(0);
-    printf("\n");
-    print_binary(1);
-    printf("\n");
-    print_binary(98);
-    printf("\n");
-    print_binary(1024);
-    printf("\n");
-    print_binary((1 << 10) + 1);
-    printf("\n");
-    return (0);
+	unsigned long int i;
+	unsigned long int digits;
+	
+	digits = 0;
+	
+	if (n == 0) 
+	{
+		printf("0");
+		return;
+	}
+
+	print_binary(n >> 1);
+	printf("%d, n & 1);
+
 }
--- bit_manipulation/_pow.c
+++ bit_manipulation/_pow.c
@@ -1,4 +1,4 @@
-#include "main.h"
+ginclude "main.h"
 #include <stdio.h>
 
 /**
