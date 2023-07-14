2cb6409cdab0c57c21c67b2c1a3fc962690c2de7
marktipton <6675@holbertonstudent.com>
2023-07-14 13:11:44 -0700

    clear bit using n & (~(1 << index))

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -2,22 +2,16 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * clear_bit - sets the value of a bit to 0 at a given index
  *
- * Return: Always 0.
+ * @n: decimal input
+ * @index: place where bit will be cleared
+ * Return: 1 if the bit was cleared an -1 if an error occurred
  */
-int main(void)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned long int n;
-
-    n = 1024;
-    clear_bit(&n, 10);
-    printf("%lu\n", n);
-    n = 0;
-    clear_bit(&n, 10);
-    printf("%lu\n", n);
-    n = 98;
-    clear_bit(&n, 1);
-    printf("%lu\n", n);
-    return (0);
+	if (index > 33)
+		return (-1);
+	*n = *n & (~(1 << index));
+	return (1);
 }
