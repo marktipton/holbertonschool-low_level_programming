0e6d4fa386263b3f7fc9c154ae98d968220afc50
marktipton <6675@holbertonstudent.com>
2023-07-14 13:45:01 -0700

    use XOR and a loop with b = b & (b - 1) to count bits flipped while converting one number to another

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -2,21 +2,35 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * count_bits - counts bits that need to be flipped
  *
- * Return: Always 0.
+ * @b: XOR of number n and m
+ *
+ * Return: number of bits flipped
  */
-int main(void)
-{
-    unsigned int n;
 
-    n = flip_bits(1024, 1);
-    printf("%u\n", n);
-    n = flip_bits(402, 98);
-    printf("%u\n", n);
-    n = flip_bits(1024, 3);
-    printf("%u\n", n);
-    n = flip_bits(1024, 1025);
-    printf("%u\n", n);
-    return (0);
+unsigned int count_bits(unsigned long int b)
+{
+	int count;
+
+	count = 0;
+	while (b > 0)
+	{
+		count++;
+		b = b & (b - 1);
+	}
+	return (count);
+}
+/**
+ * flip_bits - finds the number of bits flipped to convert number
+ *
+ * @n: first number
+ * @m: number to convert n to
+ * Return: number of bits flipped
+ */
+
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	return (count_bits(n ^ m));
 }
