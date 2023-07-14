f024f847085794889c8a83224d6b72a6279ecaff
marktipton <6675@holbertonstudent.com>
2023-07-14 13:04:24 -0700

    ?

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -10,10 +10,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-        if (index > 33)
-                return (-1);
+	if (index > 33)
+		return (-1);
 
-        *n = *n | (1 << index);
+	*n = *n | (1 << index);
 	return (1);
 	
 }
