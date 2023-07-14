e2466e6e5d4f5f8a0348fca3e33e886cbf7d7f6d
marktipton <6675@holbertonstudent.com>
2023-07-14 15:08:02 -0700

    set up file 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,22 +3,13 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * read_textfile - reads textfile and prints it to stdout
  *
- * Return: Always 0.
+ * @filename: name of file
+ * @letters: number of letters to read and print
+ * Return: 0 or number of letters
  */
-int main(int ac, char **av)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t n;
 
-    if (ac != 2)
-    {
-        dprintf(2, "Usage: %s filename\n", av[0]);
-        exit(1);
-    }
-    n = read_textfile(av[1], 114);
-    printf("\n(printed chars: %li)\n", n);
-    n = read_textfile(av[1], 1024);
-    printf("\n(printed chars: %li)\n", n);
-    return (0);
 }
