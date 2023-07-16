4c95e730a036b7d8d230ff71a8cf55574ddf8451
marktipton <6675@holbertonstudent.com>
2023-07-16 14:43:36 -0700

    added if statements for close

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,7 +2,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * main - 
+ * main - copies contents of one file into another
  *
  * @argc: argument count
  * @argv: array of arguments
@@ -19,12 +19,12 @@ int main(int argc, char *argv[])
 
 if (argc != 3)
 	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	
+
 file_from = open(argv[1], O_RDONLY);
 
 if (file_from == -1)
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	
+
 file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 if (file_to == -1)
@@ -33,9 +33,12 @@ if (file_to == -1)
 while ((num_bytes = read(file_from, buffer, sizeof(buffer))) > 0)
 	write(file_to, buffer, num_bytes);
 
-close(file_from);
-close(file_to);
+if (close(file_from) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+if (close(file_to) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 
 return (0);
+
 
 }
