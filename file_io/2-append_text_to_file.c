eb029e148456cdc2741860ea253d6fb565ec4435
marktipton <6675@holbertonstudent.com>
2023-07-16 13:39:44 -0700

    similar to last just O_APPEND instead of O_WRONLY and O_TRUNC

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
+#include <string.h>
 /**
- * main - check the code
+ * append_text_to_file - appends text to end of file
  *
- * Return: Always 0.
+ * @filename: file to append to
+ * @text_content: text to add to file
+ * Return: 1 on success and -1 on failure
  */
-int main(int ac, char **av)
+int append_text_to_file(const char *filename, char *text_content)
 {
-    int res;
+	int append_file;
+	int file_len;
+	int write_text;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = append_text_to_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+        append_file = open(filename, O_WRONLY | O_APPEND);
+        if (append_file == -1)
+                return (-1);
+
+        file_len = strlen(text_content);
+
+        write_text = write(append_file, text_content, file_len);
+
+        if (write_text == -1)
+        {
+                close(append_file);
+                return (-1);
+        }
+
+        return (1);
+
+
 }
