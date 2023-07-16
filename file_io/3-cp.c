e74af77a082fb5e1c0b226763d58fe5f4460c126
marktipton <6675@holbertonstudent.com>
2023-07-16 14:36:50 -0700

    buffer 1024

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
+/**
+ * main - 
+ *
+ * @argc: argument count
+ * @argv: array of arguments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_from;
+	int file_to;
+	char buffer[1024];
+	int num_bytes;
+
+if (argc != 3)
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	
+file_from = open(argv[1], O_RDONLY);
+
+if (file_from == -1)
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	
+file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+if (file_to == -1)
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+while ((num_bytes = read(file_from, buffer, sizeof(buffer))) > 0)
+	write(file_to, buffer, num_bytes);
+
+close(file_from);
+close(file_to);
+
+return (0);
+
+}
