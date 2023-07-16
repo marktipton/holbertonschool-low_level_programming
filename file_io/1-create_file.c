100c52f596dc8afae013f264d8bc05a7bbd8dc39
marktipton <6675@holbertonstudent.com>
2023-07-16 13:13:14 -0700

    new include statements for reading opening writing etc

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include <string.h>
 
 /**
- * main - check the code
+ * create_file - creates a file
  *
- * Return: Always 0.
+ * Return: 1 on success and -1 on failure
  */
-int main(int ac, char **av)
+int create_file(const char *filename, char *text_content)
 {
-    int res;
+	int new_file;
+	int file_len;
+	int write_text;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = create_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (new_file == -1)
+		return (-1);
+
+	file_len = strlen(text_content);
+
+	write_text = write(new_file, text_content, file_len);
+
+	if (write_text == -1)
+	{
+		close(new_file);
+		return (-1);
+	}
+
+	return (1);
+
 }
