4e7e7aa7d2e07a4c93309c3aeb5713c40291432e
marktipton <6675@holbertonstudent.com>
2023-07-17 13:21:41 -0700

    free list

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -4,25 +4,15 @@
 #include "lists.h"
 
 /**
- * main - check the code
+ * free_listint - frees a listint_t list
  *
- * Return: Always 0.
+ * @head: first node in list
+ *
  */
-int main(void)
+void free_listint(listint_t *head)
 {
-    listint_t *head;
-
-    head = NULL;
-    add_nodeint_end(&head, 0);
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
-    print_listint(head);
-    free_listint(head);
-    head = NULL;
-    return (0);
+	if (head == NULL)
+		return;
+	free_listint(head->next);
+	free(head);
 }
