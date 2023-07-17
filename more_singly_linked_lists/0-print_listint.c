34c9b0f62e0934e3fa6f0e18e49d37be405a19c7
marktipton <6675@holbertonstudent.com>
2023-07-17 12:49:23 -0700

    make list

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -11,5 +11,18 @@
  */
 size_t print_listint(const listint_t *h)
 {
+	int i;
 
+	if (h == NULL)
+		return (0);
+	for (i = 1; h->next != NULL; i++)
+	{
+		if (h->str == NULL)
+			printf("[%u] %s\n", h->len, "(nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	printf("[%u] %s\n", h->len, h->str);
+	return (i);
 }
