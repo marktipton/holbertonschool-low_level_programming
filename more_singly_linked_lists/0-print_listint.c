de864b7633fe60de1b5700ccd3a84bf531402f4f
marktipton <6675@holbertonstudent.com>
2023-07-17 07:59:39 -0700

    started 0

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -4,29 +4,12 @@
 #include "lists.h"
 
 /**
- * main - check the code
+ * print_listint - prints all elements of a listint_t list
  * 
- * Return: Always 0.
+ * @h: head node
+ * Return: number of nodes
  */
-int main(void)
+size_t print_listint(const listint_t *h)
 {
-    listint_t *head;
-    listint_t *new;
-    listint_t hello = {8, NULL};
-    size_t n;
 
-    head = &hello;
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->n = 9;
-    new->next = head;
-    head = new;
-    n = print_listint(head);
-    printf("-> %lu elements\n", n);
-    free(new);
-    return (0);
 }
