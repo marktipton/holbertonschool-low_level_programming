fbd72961cf880149d062f8264b2729ba614136e8
marktipton <6675@holbertonstudent.com>
2023-07-19 11:40:06 -0700

    either start counting at 1 and use h->next != NULL or start counting at 0 and use h != NULL

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -11,5 +11,18 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t len;
 
+	if (h == NULL)
+		return (0);
+
+	len = 1;
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+		len++;
+	}
+
+	return (len);
 }
