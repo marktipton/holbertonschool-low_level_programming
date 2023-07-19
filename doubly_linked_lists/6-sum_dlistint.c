a7f63f6fc358456c6a6b4a8b748ac294781203c6
marktipton <6675@holbertonstudent.com>
2023-07-19 13:00:19 -0700

    sum

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -11,5 +11,19 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
+	dlistint_t *current;
+	int sum;
 
+	if (head == NULL)
+		return (0);
+	current = head;
+	sum = 0;
+
+
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
 }
