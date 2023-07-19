98d77d00086dbc587ed4542f0b995e7bc6fda534
marktipton <6675@holbertonstudent.com>
2023-07-19 16:20:27 -0700

    delete nodes

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -13,5 +13,30 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	unsigned int i;
+	dlistint_t *temp;
+	dlistint_t *del;
 
+	if (head == NULL)
+		return (-1);
+
+	temp = *head;
+	i = 0;
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+	while (i < index - 1)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+			return (-1);
+	}
+
+	del = temp->next;
+	free(del);
+	return (1);
 }
