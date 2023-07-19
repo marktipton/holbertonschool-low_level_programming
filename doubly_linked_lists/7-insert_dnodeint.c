bf0aa45c00a98172ba624d608e47912f1739c0d5
marktipton <6675@holbertonstudent.com>
2023-07-19 13:49:48 -0700

    added current->next = new_node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,5 +13,33 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *new_node;
+	dlistint_t *current;
+	unsigned int count;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	if (h == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	count = 0;
+	current = *h;
+
+	while (current != NULL && count < idx)
+	{
+		count++;
+		current = current->next;
+	}
+
+	new_node->next = current->next;
+	current->next = new_node;
+	new_node->prev = current;
+
+	if (new_node->next != NULL)
+		new_node->next->prev = new_node;
+	return (new_node);
 
 }
