74ca58bc27b21b98b9ec2796079d2d81ad9b1233
marktipton <6675@holbertonstudent.com>
2023-07-19 11:32:20 -0700

    print node use h != NULL instead of h->next != NULL

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -11,5 +11,16 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t i;
 
+	if (h == NULL)
+		return (0);
+	i = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
