35494c9d581ec0f65b5bacd54829b0790eec7873
marktipton <6675@holbertonstudent.com>
2023-07-20 10:44:26 -0700

    added conditions for *h == NULL

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,15 +14,23 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node;
-	dlistint_t *current;
+	dlistint_t *tmp1;
+	dlistint_t *tmp2;
 	unsigned int count;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	if (h == NULL)
-		return (NULL);
+	if (*h == NULL)
+	{
+		if (idx != 0)
+		{
+			return (NULL);
+		}
+		*h = new_node;
+		return (new_node);
+	}
 
 	new_node->n = n;
 	count = 0;
