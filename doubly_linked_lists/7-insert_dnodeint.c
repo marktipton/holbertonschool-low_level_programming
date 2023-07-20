2ab3f527102da04bb5f23ed5405599453d4b0c7e
marktipton <6675@holbertonstudent.com>
2023-07-20 13:33:56 -0700

    trying to use add_node and add_node end functions

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,38 +17,20 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current;
 	unsigned int count;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	current = *h;
-	if (current == NULL)
-	{
-		new_node->prev = NULL;
-		new_node->next = NULL;
-		*h = new_node;
-		return (new_node);
-	}
-
 	if (idx == 0)
 	{
-		new_node->prev = NULL;
-		new_node->next = current;
-		current->prev = new_node;
-		*h = new_node;
+		new_node = add_dnodeint(h, n);
 		return (new_node);
 	}
-
 	count = 0;
 	current = *h;
 
-	while (current != NULL && count < idx - 1)
+	while (current->next != NULL && count < idx - 1)
 	{
 		current = current->next;
 		count++;
 	}
-	if (count < (idx - 1) && current == NULL)
+	if (count < (idx - 1) && current->next == NULL)
 		return (NULL);
 
 	new_node->prev = current;
