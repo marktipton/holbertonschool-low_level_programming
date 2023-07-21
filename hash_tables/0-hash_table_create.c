376cec45ae93030c5184aa92364d51819f7b62f3
marktipton <6675@holbertonstudent.com>
2023-07-21 12:18:47 -0700

    malloc hash table and hash table array

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,8 +13,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 
-	new_table = malloc(sizeof(hash_table_t) * size);
+	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
+		return (NULL);
+	
+	new_table->array = malloc(sizeof(hash_table_t) * size);
+	if (new_table->array == NULL)
 		return (NULL);
 	new_table->size = size;
 	return (new_table);
