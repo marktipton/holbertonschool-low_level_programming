1ea89b9039c132a42af3347d11d84eec6697dd18
marktipton <6675@holbertonstudent.com>
2023-07-21 14:38:20 -0700

    ht->size instead of sizeof(ht)

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -28,11 +28,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		new_node = new_node->next;
 	}
 
-	new_node = malloc(sizeof(hash_node_t));
-	
+	new_node = malloc(sizeof(hash_node_t));	
 	if (new_node == NULL)
 		return (0);
-	index = key_index((const unsigned char *)key, sizeof(ht));
+
+	index = key_index((const unsigned char *)key, ht->size);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
