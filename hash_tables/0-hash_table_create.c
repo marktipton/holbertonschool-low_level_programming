a60f75a22420a76b003592cba7055f90522b1a78
marktipton <6675@holbertonstudent.com>
2023-07-21 12:39:25 -0700

    sizeof void *

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t) * size);
+	new_table->array = malloc(sizeof(void *) * size);
 	if (new_table->array == NULL)
 	{
 		return (NULL);
