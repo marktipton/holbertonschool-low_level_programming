c02444fddefd8a10f33d2c0d48d6ad60044a13e9
marktipton <6675@holbertonstudent.com>
2023-07-21 14:25:17 -0700

    when casting put the parentheses right up against the thing you are casting

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,12 +13,30 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned int index;
+	unsigned long int index;
 	hash_node_t *new_node;
 
-	if (key == NULL)
-		return (NULL);
+	if (key == NULL || strlen(key) == 0)
+		return (0);
+	while (new_node != NULL)
+	{
+		if (strcmp(new_node->key, key) == 0)
+		{
+			new_node->value = strdup(value);
+			return (1);
+		}
+		new_node = new_node->next;
+	}
 
-	new_node = malloc(sizeof(hash_node_t);
+	new_node = malloc(sizeof(hash_node_t));
+	
+	if (new_node == NULL)
+		return (0);
+	index = hash_djb2((const unsigned char *)key) % sizeof(ht);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
+	return (1);
 }
