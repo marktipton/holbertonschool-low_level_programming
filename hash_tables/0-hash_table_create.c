19863789b1a950c3c8cb22725dbb28ca96b034ce
marktipton <6675@holbertonstudent.com>
2023-07-21 12:14:07 -0700

    handle malloc return and multiply by size in malloc

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,11 +12,10 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-       
-	new_table = malloc(sizeof(hash_table_t));
 
-	new_table->size = size;
+	new_table = malloc(sizeof(hash_table_t) * size);
 	if (new_table == NULL)
 		return (NULL);
+	new_table->size = size;
 	return (new_table);
 }
