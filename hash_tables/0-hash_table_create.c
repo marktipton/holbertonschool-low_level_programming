4a4d13913421413ac36c36292aff839f8098d9bb
marktipton <6675@holbertonstudent.com>
2023-07-21 12:25:25 -0700

    added ->table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,10 +13,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 
-	new_table = malloc(sizeof(hash_table_t));
+	new_table->table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
-	
+
 	new_table->array = malloc(sizeof(hash_table_t) * size);
 	if (new_table->array == NULL)
 		return (NULL);
