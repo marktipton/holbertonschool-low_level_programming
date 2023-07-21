9de0fc4030735edeb1882753a5f26d2e02bcfe13
marktipton <6675@holbertonstudent.com>
2023-07-21 12:35:54 -0700

    free table if array isnt allotted

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -20,6 +20,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_table_t) * size);
 	if (new_table->array == NULL)
+	{
 		return (NULL);
+		free(new_table);
+	}
 	return (new_table);
 }
