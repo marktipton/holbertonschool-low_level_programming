929bfdaf550019f417ab7aeaa26a475cb45ea21a
marktipton <6675@holbertonstudent.com>
2023-07-23 12:01:32 -0700

    prriiiiiniinnntntt

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -22,10 +22,11 @@ void hash_table_print(const hash_table_t *ht)
 		tmp = ht->array[i];
 		while (tmp != NULL)
 		{
-			printf("'%s': '%s'", tmp->key, tmp->value);
+			if (tmp->next == NULL)
+				printf("'%s': '%s'", tmp->key, tmp->value);
+			else 
+				printf("'%s': '%s', ", tmp->key, tmp->value);
 			tmp = tmp->next;
-			if (tmp != NULL)
-				printf(", ");
 		}
 
 	}
