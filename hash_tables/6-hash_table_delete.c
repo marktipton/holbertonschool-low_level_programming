a425eee3e9c98765d592449f578007078e072b72
marktipton <6675@holbertonstudent.com>
2023-07-23 13:18:02 -0700

    delete table compiles now

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -17,17 +17,16 @@ void hash_table_delete(hash_table_t *ht)
 		return;
 
 	i = 0;
-	tmp = ht->array[index];
 
 	while (i < ht->size)
 	{
 		while (ht->array[i] != NULL)
 		{
 			tmp = ht->array[i]->next;
-			free(ht->array[i]->key;
-			free(ht->array[i]->value;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
 			free(ht->array[i]);
-			ht->array[i] = next;
+			ht->array[i] = tmp;
                 }
                 i++;
 	}
