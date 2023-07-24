1cd65cb287d939762bffdcee689efad1289e67a3
marktipton <6675@holbertonstudent.com>
2023-07-24 15:42:29 -0700

    changed logic in delete doubly linked

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -4,7 +4,7 @@
 #include "lists.h"
 
 /**
- * delete_dnode_int_at_index - delete node at index
+ * delete_dnodeint_at_index - delete node at index
  *
  * @head: pointer to head node
  * @index: index of node to be deleted
@@ -23,13 +23,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (index == 0 && *head != NULL)
 	{
-		if ((*head)->next != NULL)
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
 		{
 			*head = (*head)->next;
 			free(temp);
 		}
-		else
-			*head = NULL;
+		free(del);
 		return (1);
 	}
 	while (i < index - 1 && del != NULL)
@@ -37,10 +38,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		del = del->next;
 		i++;
 	}
-	if (del != NULL)
-	{
-		free(del);
-		return (1);
-	}
-	return (-1);
+	if (del == NULL)
+		return (-1);
+	temp = del->prev;
+	temp->next = del->next;
+	if (del->next != NULL)
+		del->next->prev = temp;
+	free(del);
+	return (1);
+
 }
