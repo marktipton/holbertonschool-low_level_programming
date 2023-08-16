738980e4854e9d2754d7ec9e1ea6a80f5b07b448
marktipton <6675@holbertonstudent.com>
2023-08-16 10:09:49 -0700

    linear search with print statement

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,6 +1,6 @@
 #include "search_algos.h"
 /**
- * binary_search - searches for a value in a sorted array of integers
+ * linear_search - searches for a value in a sorted array of integers
  *
  * @array: pointer to first element of array to search
  * @size: number of elements in array
@@ -8,7 +8,19 @@
  * Return: -1 if value not present or first index where value is present
  *
  */
-int binary_search(int *array, size_t size, int value)
+int linear_search(int *array, size_t size, int value)
 {
-	
+	size_t i = 0;
+
+	while (i < size)
+	{
+		if (array[i] == value)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+			return (i);
+		}
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		i++;
+	}
+	return (-1);
 }
