ea5c881c7c7be6e915d02e03e6aa7223c6571d68
marktipton <6675@holbertonstudent.com>
2023-08-16 10:32:47 -0700

    first draft of binary search

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search -
+ * binary_search - searches for value in sorted array of integers
  *
  * @array: pointer to first element of the array
  * @size: number of elements in array
@@ -11,5 +11,20 @@
 
 int binary_search(int *array, size_t size, int value)
 {
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t middle;
 
+	while (left <= right)
+	{
+		middle = left + (right - left) / 2;
+
+		if (array[middle] == value)
+			return middle;
+		else if (array[middle] < value)
+			left = middle + 1;
+		else
+			right = middle - 1;
+	}
+	return (-1);
 }
