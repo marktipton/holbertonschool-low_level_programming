3e4ef00d92bddb42b8ae59c699b9cae882ae162d
marktipton <6675@holbertonstudent.com>
2023-08-16 09:42:00 -0700

    made files

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ *
+ * @array: pointer to first element of array to search
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: -1 if value not present or first index where value is present
+ *
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	
+}
---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+
+/**
+ * binary_search -
+ *
+ * @array: pointer to first element of the array
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: -1 if value not found or index where value is located
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+
+}
